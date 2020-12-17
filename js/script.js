f9b536857d559fe0832887504e0d5e3f68cfd464
sqwa11 <65025160+sqwa11@users.noreply.github.com>
2020-12-18 05:08:05 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const menuBtn = document.querySelector(".m-menu");
+const menu =document.querySelector(".menu-sidebar");
+menuBtn.addEventListener('click', function(){
+    menu.classList.toggle("active");
+})
